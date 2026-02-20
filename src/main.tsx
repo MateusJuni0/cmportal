b4ee986942b28364e59571a2b64af885475253e6
MateusJuni0 <mateus.junior.oliveira.mjo@gmail.com>
2026-02-20 21:18:23 +0000

    Fix: Remove TSC from build to unblock Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log('💎 CMTEC Sovereign Engine V4 Online 💎')
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
